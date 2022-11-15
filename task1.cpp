3b14a35ba14a0cdfd9224693ba3933138bce2a22
Кирилл <112952627+motorollainlinux@users.noreply.github.com>
2022-11-15 16:44:23 +0700

    Updated task1 for c11 style guide

--- task1.cpp
+++ task1.cpp
@@ -1,19 +1,16 @@
 
 #include <iostream>
 
-using namespace std;
 void BiggerNumber(int &Result) {
-     int num1, num2;
-     cin >> num1>>num2;
-     if (num1 > num2) {
-         Result = num1;
-     } else {
-         Result = num2;
-     }
+    int num1, num2;
+    std::cin >> num1>>num2;
+    if (num1 > num2)
+    Result = num1;
+    else
+    Result = num2;
 }
 int main() {
-     int Result;
-     BiggerNumber(Result);
-     cout << Result;
-     return 0;
+    int Result;
+    BiggerNumber(Result);
+    std::cout << Result;
 }
